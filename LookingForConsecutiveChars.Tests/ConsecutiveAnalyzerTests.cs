2a38f36e14fc19a72e1965a9ef13c672cc2071c7
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2024-04-26 21:54:46 +0300

    Add project files.

---
+++ LookingForConsecutiveChars.Tests/ConsecutiveAnalyzerTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using LookingForConsecutiveChars;
+
+namespace LookingForConsecutiveChars.Tests
+{
+    [TestFixture]
+    public class ConsecutiveAnalyzerTests
+    {
+        [Test]
+        public void GetQuantityOfMaxConsecutiveLetters_ParameterIsNull_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveLetters(null));
+        }
+
+        [TestCase("", ExpectedResult = 0)]
+        public int GetQuantityOfMaxConsecutiveLetters_ParameterIsEmptyString_ReturnsZero(string str)
+        {
+            return ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveLetters(str);
+        }
+
+        [TestCase("12234566", ExpectedResult = 0)]
+        [TestCase(",...,??", ExpectedResult = 0)]
+        public int GetQuantityOfMaxConsecutiveLetters_ParameterIsStringWithoutLetters_ReturnsZero(string str)
+        {
+            return ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveLetters(str);
+        }
+
+        [TestCase("a", ExpectedResult = 1)]
+        [TestCase("aaabbc99999", ExpectedResult = 3)]
+        [TestCase("AaAaAacccccbd ЖЖЖЖЖЖЖ", ExpectedResult = 5)]
+        [TestCase("bc          LLLLLL", ExpectedResult = 6)]
+        [TestCase("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm", ExpectedResult = 100)]
+        public int GetQuantityOfMaxConsecutiveLetters_ParameterIsValid_ReturnsCorrectQuantity(string str)
+        {
+            return ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveLetters(str);
+        }
+
+        [Test]
+        public void GetQuantityOfMaxConsecutiveDigits_ParameterIsNull_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveDigits(null));
+        }
+
+        [TestCase("", ExpectedResult = 0)]
+        public int GetQuantityOfMaxConsecutiveDigits_ParameterIsEmptyString_ReturnsZero(string str)
+        {
+            return ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveDigits(str);
+        }
+
+        [TestCase("Hello, world!", ExpectedResult = 0)]
+        [TestCase("AaBbCc +-^^", ExpectedResult = 0)]
+        public int GetQuantityOfMaxConsecutiveDigits_ParameterIsStringWithoutDigits_ReturnsZero(string str)
+        {
+            return ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveDigits(str);
+        }
+
+        [TestCase("1", ExpectedResult = 1)]
+        [TestCase("122333444455555 nnnnnnnnnnnnn", ExpectedResult = 5)]
+        [TestCase("11112 bc 111111", ExpectedResult = 6)]
+        [TestCase("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", ExpectedResult = 100)]
+        public int GetQuantityOfMaxConsecutiveDigits_StrParameterIsValid_ReturnsCorrectQuantity(string str)
+        {
+            return ConsecutiveAnalyzer.GetQuantityOfMaxConsecutiveDigits(str);
+        }
+    }
+}
---
+++ LookingForConsecutiveChars/ConsecutiveAnalyzer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace LookingForConsecutiveChars
+{
+    public class ConsecutiveAnalyzer
+    {
+        public static int GetQuantityOfMaxConsecutiveLetters(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+            if (str.Length == 0)
+            {
+                return 0;
+            }
+
+            int maxCount = 0;
+            int currentCount = 1;
+            bool IsAnyLetters = false;
+            char[] latinAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char prevChar = '\0';
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (latinAlphabet.Contains(str[i]))
+                {
+                    IsAnyLetters = true;
+                    if (str[i] == prevChar)
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        maxCount = Math.Max(maxCount, currentCount);
+                        currentCount = 1;
+                    }
+                    prevChar = str[i];
+                }
+            }
+
+            if (!IsAnyLetters)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Max(maxCount, currentCount);
+            }
+        }
+        public static int GetQuantityOfMaxConsecutiveDigits(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+            if (str.Length == 0)
+            {
+                return 0;
+            }
+
+            int maxCount = 0;
+            int currentCount = 1;
+            bool IsAnyDigits = false;
+            char prevChar = '\0';
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsDigit(str[i]))
+                {
+                    IsAnyDigits = true;
+                    if (str[i] == prevChar)
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        maxCount = Math.Max(maxCount, currentCount);
+                        currentCount = 1;
+                    }
+                    prevChar = str[i];
+                }
+            }
+
+            if (!IsAnyDigits)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Max(maxCount, currentCount);
+            }
+        }
+    }
+}
